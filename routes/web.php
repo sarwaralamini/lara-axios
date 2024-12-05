4bfd862e1803b928492bb3a7b7ea47c5c73eed1e
Md. Sarwar Alam <sarwaralam.ini@gmail.com>
2024-12-06 00:21:48 +0600

    Refactor routes to group with 'web.auth' middleware for better readability and maintainability.

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('redirect.auth')->name('home');
 
-// Route::get('dashboard', function () {
-//     return view('dashboard');
-// })->name('dashboard');
+Route::middleware('web.auth')->group(function () {
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::get('dashboard', function () {
-    return view('dashboard');
-})->middleware('web.auth')->name('dashboard');
-
-Route::get('user/create', function () {
-    return view('create');
-})->middleware('web.auth')->name('user.create');
+    Route::get('user/create', function () {
+        return view('create');
+    })->name('user.create');
+});
